45c1e4261168995e5770a4d3177f57bd71b462d3
71walceli <antonini2903@hotmail.com>
2022-03-27 12:16:09 -0500

    Code refactoring and reorganization - Renamed download_site_files -> download_files - Renamed FtpClient.cloneFtp -> FtpClient.cloneFolder - Moved list_files_folders into FtpClient class

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import ftplib
 import json
 from functools import reduce
 import os, sys, os.path, shutil, operator
-import utils
 
 
 LOCAL_FTP_DIRECTORY = "./ftp"
@@ -28,55 +27,71 @@ class FtpClient(ftplib.FTP):
     self.login(self.user, self.password)
     self.cwd(previous_path)
 
-# Adapted from https://stackoverflow.com/a/55127679
-def cloneFtp(ftp_connection, remote_dir, local_dir):
-  # TODO Add counters to count identified files vs downloaded ones count.
-  try: 
-    shutil.rmtree(local_dir)
-    pass
-  except: pass
-  try: 
-    os.makedirs(local_dir)
-    pass
-  except: pass
-  files, dirs = utils.list_files_folders(ftp_connection, remote_dir)
-  for file in files:
-    local_file_path = os.path.join(local_dir, file)
-    remote_file_path = f"{remote_dir}/{file}"
-    while True: 
-      try:
-        ftp_connection.retrbinary('RETR '+ remote_file_path, open(local_file_path, 'wb').write)
-        break
-      except TimeoutError as exception:
-        continue # Might happen on slow or unreliable connections
-      except KeyboardInterrupt as exception:
-        raise # Respond to CTRK+C here, given that caller won't be able to respond
-      except BaseException as exception:
-        print(f"Error [{exception}]: {remote_file_path}")
-        continue
-  #ftp_connection.reconnect()
-  def recurse(dir): 
-    #try:
-    #  ftp.cwd(remote_path)
-    #except: 
-    #  print(f"[CWD Error] {remote_path}")
-    remote_path = f"{remote_dir}/{dir}"
-    local_path = os.path.join(local_dir, dir)
-    return cloneFtp(ftp_connection, remote_path, local_path)
-  res = map(recurse, dirs)
-  return reduce(operator.iand, res, True)
+  def list_files_folders(self, directory: str) -> tuple[list, list]:
+    """List all files and folders in specified directory."""
+    files, dirs = [], []
+    def classify(listing: str):
+      listing = listing.split(maxsplit=8)
+      listing_type = listing[0][0]
+      node = listing[8]
+      if node in [".", ".."]:
+        return
+      if listing_type == "d":
+        dirs.append(node)
+      else:
+        files.append(node)
+    self.dir(directory, classify)
+    return files, dirs
+
+  # Adapted from https://stackoverflow.com/a/55127679
+  def cloneFolder(self, remote_dir, local_dir):
+    # TODO Add counters to count identified files vs downloaded ones count.
+    try: 
+      shutil.rmtree(local_dir)
+      pass
+    except: pass
+    try: 
+      os.makedirs(local_dir)
+      pass
+    except: pass
+    files, dirs = self.list_files_folders(remote_dir)
+    for file in files:
+      local_file_path = os.path.join(local_dir, file)
+      remote_file_path = f"{remote_dir}/{file}"
+      while True: 
+        try:
+          self.retrbinary('RETR '+ remote_file_path, open(local_file_path, 'wb').write)
+          break
+        except TimeoutError as exception:
+          continue # Might happen on slow or unreliable connections
+        except KeyboardInterrupt as exception:
+          raise # Respond to CTRK+C here, given that caller won't be able to respond
+        except BaseException as exception:
+          print(f"Error [{exception}]: {remote_file_path}")
+          continue
+    #ftp_connection.reconnect()
+    def recurse(dir): 
+      #try:
+      #  ftp.cwd(remote_path)
+      #except: 
+      #  print(f"[CWD Error] {remote_path}")
+      remote_path = f"{remote_dir}/{dir}"
+      local_path = os.path.join(local_dir, dir)
+      return self.cloneFolder(remote_path, local_path)
+    res = map(recurse, dirs)
+    return reduce(operator.iand, res, True)
 
 
 def download_database(ftp_credentials, local_dir):
   pass
 
 
-def download_site_files(ftp_credentials, local_dir):
+def download_files(ftp_credentials, local_dir):
   with FtpClient(ftp_credentials["host"], ftp_credentials["user"], ftp_credentials["password"]) \
   as ftp:
     ftp.cwd(ftp_credentials["directory"])
     print(f"Downloading directory for {domain} into {local_dir}")
-    cloneFtp(ftp, ftp_credentials["directory"], local_dir)
+    ftp.cloneFolder(ftp_credentials["directory"], local_dir)
 
 
 with open("./websitesData.json") as jsonData:
@@ -89,6 +104,6 @@ if __name__ == "__main__":
     ftp_credentials = credentials["ftpCredentials"]
     mysql_credentials = credentials["mysqlDbCredentials"]
     #download_database(mysql_credentials, os.path.join(LOCAL_FTP_DIRECTORY, domain))
-    download_site_files(ftp_credentials, os.path.join(LOCAL_FTP_DIRECTORY, domain))
+    download_files(ftp_credentials, os.path.join(LOCAL_FTP_DIRECTORY, domain))
 
 
